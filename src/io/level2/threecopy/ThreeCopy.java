f94066bcee7dfa522570aba5e49553ee3bd02672
Alex.Racov <alex.racov@gmail.com>
2020-02-17 19:09:53 -0700

    module2. level2. optimize ThreeCopy

--- src/io/level2/threecopy/ThreeCopy.java
+++ src/io/level2/threecopy/ThreeCopy.java
@@ -13,23 +13,10 @@ public class ThreeCopy {
         Path file1 = Paths.get ( "src/io/level2/threecopy/file1.txt" );
         Path file2 = Paths.get ( "src/io/level2/threecopy/file2.txt" );
         Path file3 = Paths.get ( "src/io/level2/threecopy/file3.txt" );
-
-        //        delete files if necessary and re-create
-        try {
-            Files.deleteIfExists ( file1 );
-            Files.deleteIfExists ( file2 );
-            Files.deleteIfExists ( file3 );
-            Files.createFile ( file1 );
-            Files.createFile ( file2 );
-            Files.createFile ( file3 );
-        } catch (IOException e) {
-            e.printStackTrace ( );
-        }
-
         try (BufferedReader inStream = Files.newBufferedReader ( Paths.get ( "C:\\Users\\alex.racov\\IdeaProjects\\module2\\src\\io\\level1\\Go.dat" ) );
-             BufferedWriter bw1 = new BufferedWriter ( new FileWriter ( file1.toFile ( ) , true ) );
-             BufferedWriter bw2 = new BufferedWriter ( new FileWriter ( file2.toFile ( ) , true ) );
-             BufferedWriter bw3 = new BufferedWriter ( new FileWriter ( file3.toFile ( ) , true ) );
+             BufferedWriter bw1 = new BufferedWriter ( new FileWriter ( file1.toFile ( ) ) );
+             BufferedWriter bw2 = new BufferedWriter ( new FileWriter ( file2.toFile ( ) ) );
+             BufferedWriter bw3 = new BufferedWriter ( new FileWriter ( file3.toFile ( )  ) );
         ) {
             String line;
             while (( line = inStream.readLine ( ) ) != null) {
